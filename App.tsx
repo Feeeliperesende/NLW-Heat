6d8dfd67c3cdd0123d0f99bd24a4764701a11997
Feeeliperesende <felipe_kid_13@hotmail.com>
2021-10-24 15:43:36 -0300

    cont-heat

--- App.tsx
+++ App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-
+import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import AppLoading from 'expo-app-loading';
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
+  );
 }
